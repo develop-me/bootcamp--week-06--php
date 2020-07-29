c78bb202763210a03b04cef8a153f0f0f6069e02
Mark Wales <mark@developme.tech>
2020-07-29 17:10:48 +0100

    notes: removes old notes

--- notes/04/figures/01/15-MailerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface MailerInterface
-{
-  public function to(string $address) : MailerInterface;
-  public function from(string $address) : MailerInterface;
-  public function send(string $sub, string $msg) : MailerInterface;
-}
