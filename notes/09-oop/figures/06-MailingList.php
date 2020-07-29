8383e6ed1bc47c3cc4556b9381b6605da2bbb29d
Mark Wales <mark@developme.tech>
2020-07-29 09:40:56 +0100

    few tweaks to notes

--- notes/09-oop/figures/06-MailingList.php
+++ notes/09-oop/figures/06-MailingList.php
@@ -2,17 +2,22 @@
 
 class MailingList
 {
-    private $subscribers = [];
+  private $subscribers = [];
 
-    public function __construct($subscribers)
-    {
-        $this->subscribers = $subscribers;
-    }
+  public function __construct($subscribers)
+  {
+    $this->subscribers = $subscribers;
+  }
 
-    public function send($mail)
-    {
-        foreach ($this->subscribers as $subscriber) {
-            $mail->to($subscriber)->mail();
-        }
+  // the send method takes a mail object as an argument
+  public function send($mail)
+  {
+    // go through each subscriber one at a time
+    foreach ($this->subscribers as $subscriber) {
+      // use the passed in Mail object
+      // update just the to field each time
+      // then send the mail
+      $mail->to($subscriber)->mail();
     }
+  }
 }
